a87190e17429b85442aa6a2e301819583e722ec6
Mariam-zain1 <mariamzain293@gmail.com>
2026-07-30 23:06:49 +0300

    Solving the first question

--- Proplem1/src/StudentInformationCard.java
+++ Proplem1/src/StudentInformationCard.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class StudentInformationCard {
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your full name:");
+        String Name=scanner.nextLine();
+        System.out.println("Enter your Age:");
+        int Age =scanner.nextInt();
+        System.out.println("Enter your University:");
+        String Uni=scanner.nextLine();
+        System.out.println("Enter your Gpa:");
+        double Gpa=scanner.nextDouble();
+        System.out.println("=========Student card=========");
+        System.out.println("Full Name:"+Name);
+        System.out.println("Age:"+Age);
+        System.out.println("University:"+Uni);
+        System.out.println("Gpa"+Gpa);
+        System.out.println("=======================");
     }
 }
